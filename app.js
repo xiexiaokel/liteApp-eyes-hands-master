c05bd69040b542ae59e3a7930bae748206267e1a
xiexiaokel <xiexiaokel@sina.com>
2018-04-18 17:08:00 +0800

    首页修改

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ App({
   onLaunch: function () {
   },
   globalData: {
-    //imgHost: 'https://duokeyun.oss-cn-qingdao.aliyuncs.com/wechat/dk',
     imgHost: 'https://duokeyun.oss-cn-qingdao.aliyuncs.com/wechat/wxapp/eyes_hands'
   }
 })
